bf14cebe2a16c1e2b714fe82d27eae42550cf453
Valdênio Martinho Pereira <valdeniomartinho@gmail.com>
2021-02-28 08:46:49 -0300

    iniciando

---
+++ Welcome4.java
@@ -0,0 +1,16 @@
+// Pacotes de extensão de Java
+import javax.swing.JOptionPane; // importa a classe JOptionPane
+
+public class Welcome4 {
+
+    //método main começa a execução do aplicativo Java
+    public static void main(String args[]) {
+      JOptionPane.showMessageDialog(
+          null, "Welcome\nto\nJava\nProgramming!"
+      );
+       System.exit(0); // termina aplicativo
+
+    } // fim do método main
+        
+    
+} // fim da classe Welcome4
